a35084f49b8d615a2821df29dc6ae2001acb611f
Arjun <arjun8c@gmail.com>
2026-09-18 12:19:31 +0000

    Configure S3 remote state backend

--- providers.tf
+++ providers.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket       = "project-3-guvi-tfstate-cdf5fc5a"
+    key          = "project-3-guvi/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
